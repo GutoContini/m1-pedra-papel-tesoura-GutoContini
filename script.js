ad27c0cc61c1cf1923ca2be80915c9c500c092b4
Augusto <augusto.silva.contini@hotmail.com>
2024-07-15 16:31:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+    if (
+      (player1 === "Pedra" && player2 === "Tesoura") ||
+      (player1 === "Papel" && player2 === "Pedra") ||
+      (player1 === "Tesoura" && player2 === "Papel")
+    ) {
+      resultado = "Jogador 1 venceu!";
+    } else if (
+      (player1 === "Tesoura" && player2 === "Pedra") ||
+      (player1 === "Pedra" && player2 === "Papel") ||
+      (player1 === "Papel" && player2 === "Tesoura")
+    ) {
+      resultado = "Jogador 2 venceu!";
+    } else {
+      resultado = "Empate!";
+    }
+    return resultado;
+  }
